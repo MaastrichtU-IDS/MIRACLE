d3b5b53e0da389d5f89ccb420e42ce6a54cba94a
ebylmz <ebylmz17@gmail.com>
2023-08-12 11:23:56 +0200

    combine training and dataset statistics

--- scripts/analyse.sh
+++ scripts/analyse.sh
@@ -1,1 +1,1 @@
-python src/analyse.py ner_indications_correct labels.txt data/statistics.csv
+python src/analyse.py data/ner_indications_correct_model/model-best labels.txt ner_indications_correct data/statistics.csv
--- src/analyse.py
+++ src/analyse.py
@@ -1,7 +1,10 @@
 import sys
+import os
+import json
 import csv
 from collections import Counter
 from prodigy.components.db import connect
+from prodigy.util import get_labels
 
 def process_db(db_name: str, counts: Counter):
     db = connect()
@@ -16,22 +19,39 @@ def process_db(db_name: str, counts: Counter):
     return total
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        sys.exit(f"Right usage: {sys.argv[0]} [in: dataset-name] [in: labels.txt] [out: label_dist.csv]") 
+    if len(sys.argv) != 5:
+        sys.exit(f"Right usage: {sys.argv[0]} [in: model_dir_path] [in: labels.txt] [in: training-dataset] [out: label_dist.csv]") 
 
+    # Read the model meta data
+    model_path = os.path.join(sys.argv[1], "meta.json")
+
+    with open(model_path) as file:
+        performances = json.load(file)['performance']['ents_per_type']
+
+    # Initialize a Counter object with the labels
     counts = Counter()
-    with open(sys.argv[2]) as file:
-        for line in file:
-            # Remove the new line and initialize the label
-            counts[line.strip()] = 0
+    for label in get_labels(sys.argv[2]):
+        counts[label] = 0
 
-    total = process_db(sys.argv[1], counts)
+    total = process_db(sys.argv[3], counts)
+
+    perf_default = {'p': 0.0, 'r': 0.0, 'f': 0.0}
+
+    # Sort the counts in decreasing order
     counts_sorted = sorted(counts.items(), key=lambda item: item[1], reverse=True)
 
-    # Write the results into a CSV file
-    with open(sys.argv[3], 'w', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, ['label', 'count', 'distribution'])
+    # Write the results into given CSV file
+    with open(sys.argv[4], 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, ['Label', 'Count', 'Distribution', 'Precision', 'Recall', 'F1Score'])
         writer.writeheader()
-        writer.writerows({'label': label, 'count': count, 'distribution': round((count / total) * 100, 2)} for label, count in counts_sorted)
 
-    print(f'Total # annotation {total}. Statistics are saved in "{sys.argv[3]}"')
+        writer.writerows({
+            'Label': label, 
+            'Count': count, 
+            'Distribution': round((count / total) * 100, 2),
+            'Precision': round(performances.get(label, perf_default)['p'] * 100, 2), 
+            'Recall': round(performances.get(label, perf_default)['r'] * 100, 2), 
+            'F1Score': round(performances.get(label, perf_default)['f'] * 100, 2)
+            } for label, count in counts_sorted)
+
+    print(f'Total # annotation is {total}. Statistics saved in "{sys.argv[4]}"')
