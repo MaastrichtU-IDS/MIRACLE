f3548c8a7a615c629cf1f89f979595041623ced3
ebylmz <ebylmz17@gmail.com>
2023-08-18 14:50:32 +0200

    update path

--- scripts/analyse.sh
+++ scripts/analyse.sh
@@ -1,1 +1,1 @@
-python src/analyse.py data/ner_indications_correct_model/model-best labels.txt ner_indications_correct data/statistics.csv
+python src/analyse.py data/ner_indications_model/model-last labels.txt ner_indications_correct data/statistics.csv
