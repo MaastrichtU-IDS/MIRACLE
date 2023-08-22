8ba722ea27bc7ad99f354a191dfda312dbae3acf
ebylmz <ebylmz17@gmail.com>
2023-08-22 15:49:02 +0200

    jsonl converter for prodigy

---
+++ src/csv2jsonl.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert CSV to JSONL')
+
+    parser.add_argument('--input_csv', help='Path to input CSV file')
+    parser.add_argument('--output_jsonl', help='Path to output JSONL file')
+
+    args = parser.parse_args()
+
+    jsonl_file = args.output_jsonl
+    df = pd.read_csv(args.input_csv, usecols=['text'])
+    df.to_json(jsonl_file, orient='records', lines=True, force_ascii=False)
+
+    print(f"Conversion complete. 'text' column extracted and written to '{jsonl_file}'")
