1207f5cd84c1c9b56b1497447a2e369ee5085f5f
ebylmz <ebylmz17@gmail.com>
2023-08-22 15:48:23 +0200

    add argparse

--- scripts/analyse.sh
+++ scripts/analyse.sh
@@ -1,1 +1,6 @@
-python src/analyse.py data/ner_indications_model/model-last labels.txt ner_indications_correct data/statistics.csv
+#!/bin/bash
+
+dataset_name="ner_indications"
+output_file="data/model_training_statistics.csv"
+
+python src/analyse.py model/model-last labels.txt "$dataset_name" "$output_file" 
--- src/analyse.py
+++ src/analyse.py
@@ -1,7 +1,7 @@
-import sys
 import os
 import json
 import csv
+import argparse
 from collections import Counter
 from prodigy.components.db import connect
 from prodigy.util import get_labels
@@ -11,7 +11,6 @@ def process_db(db_name: str, counts: Counter):
     examples = db.get_dataset(db_name)
     total = 0
     for eg in examples:
-        # Access the annotated labels
         eg_labels = [span["label"] for span in eg.get("spans", [])]
         for label in eg_labels:
             counts[label] += 1
@@ -19,39 +18,40 @@ def process_db(db_name: str, counts: Counter):
     return total
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(f"Right usage: {sys.argv[0]} [in: model_dir_path] [in: labels.txt] [in: training-dataset] [out: label_dist.csv]") 
+    parser = argparse.ArgumentParser(description="Generate label distribution statistics.")
+    parser.add_argument("model_dir_path", help="Path to the model directory")
+    parser.add_argument("training_dataset", help="Name of the training dataset")
+    parser.add_argument("labels_file", help="Path to the labels.txt file")
+    parser.add_argument("output_csv", help="Path to the output CSV file")
 
-    # Read the model meta data
-    model_path = os.path.join(sys.argv[1], "meta.json")
+    args = parser.parse_args()
+
+    model_path = os.path.join(args.model_dir_path, "meta.json")
 
     with open(model_path) as file:
         performances = json.load(file)['performance']['ents_per_type']
 
-    # Initialize a Counter object with the labels
     counts = Counter()
-    for label in get_labels(sys.argv[2]):
+    for label in get_labels(args.labels_file):
         counts[label] = 0
 
-    total = process_db(sys.argv[3], counts)
+    total = process_db(args.training_dataset, counts)
 
     perf_default = {'p': 0.0, 'r': 0.0, 'f': 0.0}
 
-    # Sort the counts in decreasing order
     counts_sorted = sorted(counts.items(), key=lambda item: item[1], reverse=True)
 
-    # Write the results into given CSV file
-    with open(sys.argv[4], 'w', newline='') as csvfile:
+    with open(args.output_csv, 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, ['Label', 'Count', 'Distribution', 'Precision', 'Recall', 'F1Score'])
         writer.writeheader()
 
         writer.writerows({
-            'Label': label, 
-            'Count': count, 
+            'Label': label,
+            'Count': count,
             'Distribution': round((count / total) * 100, 2),
-            'Precision': round(performances.get(label, perf_default)['p'] * 100, 2), 
-            'Recall': round(performances.get(label, perf_default)['r'] * 100, 2), 
+            'Precision': round(performances.get(label, perf_default)['p'] * 100, 2),
+            'Recall': round(performances.get(label, perf_default)['r'] * 100, 2),
             'F1Score': round(performances.get(label, perf_default)['f'] * 100, 2)
-            } for label, count in counts_sorted)
+        } for label, count in counts_sorted)
 
-    print(f'Total # annotation is {total}. Statistics saved in "{sys.argv[4]}"')
+    print(f'Total # annotation is {total}. Statistics saved in "{args.output_csv}"')
