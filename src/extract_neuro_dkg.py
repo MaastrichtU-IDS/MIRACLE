2d347669054dee531d789d60fa5ee79766269c43
ebylmz <ebylmz17@gmail.com>
2023-09-05 17:03:32 +0200

    extract neuro_dkg relations

--- src/extract_neuro_dkg.py
+++ src/extract_neuro_dkg.py
@@ -3,116 +3,220 @@ import csv
 from rdflib import Graph, Namespace
 from rdflib.plugins.sparql import prepareQuery
 
-def get_indication_text(g_indications, context_url, nkgi_indications, rdfs):
-    context_number = context_url.split("/")[-1]
+def get_response_first(qresult, label):
+    li = list(qresult)
+    return li[0][label] if li else None
 
+def get_rdf_label(g_neuro_dkg, rdfs, url):
+    if url is None:
+        return None
+    elif not url.startswith("http://"):
+        return url
     query = prepareQuery(
         """
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        SELECT ?indication_text
+        SELECT ?label
         WHERE {
-            nkgi_indications:%s rdfs:label ?indication_text .
+            <%s> rdfs:label ?label .
         }
-        """ % context_number,
-        initNs={"nkgi_indications": nkgi_indications}
-    )
-
-    result = g_indications.query(query)
-    return list(result)[0]["indication_text"] if result else "Unknown"
-
-def get_target_group(g_neuro_dkg, target_group_url, ns1):
-    query = prepareQuery(
-        """
-        PREFIX ns1: <http://www.w3id.org/>
-
-        SELECT ?ageGroup
-        WHERE {
-            <%s> ns1:neurodkg:hasAgeGroup ?ageGroup .
-        }
-        """ % target_group_url,
-        initNs={"ns1": ns1}
-    )
-
-    result = g_neuro_dkg.query(query)
-    return list(result)[0]["ageGroup"] if result else "Unknown" 
-
-
-def get_disease_name(g_neuro_dkg, disease_url, rdfs):
-    if not disease_url.startswith("http://"):
-        return disease_url
-
-    query = prepareQuery(
-        """
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-        SELECT ?diseaseName
-        WHERE {
-            <%s> rdfs:label ?diseaseName .
-        }
-        """ % disease_url,
+        """ % url,
         initNs={"rdfs": rdfs}
     )
 
-    result = g_neuro_dkg.query(query)
-    return list(result)[0]["diseaseName"] if result else "Unknown" 
+    return get_response_first(g_neuro_dkg.query(query), "label") 
 
-def get_drug_name(g_neuro_dkg, drug_url, ns1, rdfs):
-    drug_id = drug_url.split("/")[-1]
+def get_indication_text(g_indications, context_no, nkgi_indications, rdfs):
+    if context_no is None:
+        return None
 
     query = prepareQuery(
         """
-        PREFIX ns1: <http://www.w3id.org/>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-        SELECT ?drugName
+        SELECT ?indicationText
         WHERE {
-            ns1:%s rdfs:label ?drugName .
+            nkgi_indications:%s rdfs:label ?indicationText .
         }
-        """ % drug_id,
-        initNs={"ns1": ns1, "rdfs": rdfs}
+        """ % context_no,
+        initNs={"nkgi_indications": nkgi_indications, "rdfs": rdfs}
     )
 
-    result = g_neuro_dkg.query(query)
-    return list(result)[0]["drugName"] if result else "Unknown" 
+    return get_response_first(g_indications.query(query), "indicationText")
 
-def parse(g_neuro_dkg, g_neuro_dkg_biolink, g_indications, output_csv):
-    nkgi = Namespace("https://w3id.org/neurodkg/instance/")
+
+def get_diseases(g_neuro_dkg, rdfs, disease_urls):
+    diseases = set()
+    for url in disease_urls:
+        disease = get_rdf_label(g_neuro_dkg, rdfs, url)
+        diseases.add(str(disease))
+    
+    return diseases.pop() if len(diseases) == 1 else diseases   
+
+
+def get_treatment_group(g_neuro_dkg, target_group, ns1, rdfs):
+    for treatment in target_group['TREATMENT_CODE']:
+        url = "<http://www.w3id.org/neurodkg/Instances/" + treatment + ">"
+
+        query = prepareQuery(
+            """
+            SELECT ?hasAgeGroup ?hasMinAge ?responseStatus ?hasCurrentMedication ?hasComorbidity ?treatmentDuration ?hasSymptom ?hasTherapy
+            WHERE {
+                OPTIONAL { %s ns1:neurodkg:hasAgeGroup ?hasAgeGroup }
+                OPTIONAL { %s ns1:neurodkg:hasMinAge ?hasMinAge }
+            
+                OPTIONAL { %s ns1:neurodkg:hasCurrentMedication ?hasCurrentMedication }
+                OPTIONAL { %s ns1:neurodkg:responseStatus ?responseStatus }
+                OPTIONAL { %s ns1:neurodkg:hasComorbidity ?hasComorbidity }
+
+                OPTIONAL { %s ns1:neurodkg:treatmentDuration ?treatmentDuration }
+                OPTIONAL { %s ns1:neurodkg:hasSymptom ?hasSymptom }
+                OPTIONAL { %s ns1:neurodkg:hasTherapy ?hasTherapy }
+            }
+            """ % (url, url, url, url, url, url, url, url),
+            initNs={"ns1": ns1}
+        )
+
+        for response in g_neuro_dkg.query(query):
+            if response['hasAgeGroup']:
+                target_group['AGE_GROUP'].append(response['hasAgeGroup']) 
+            if response['hasMinAge']:
+                target_group['MIN_AGE'].append(response['hasMinAge']) 
+            if response['responseStatus']:
+                target_group['RESPONSE_STATUS'].append(response['responseStatus']) 
+            if response['hasCurrentMedication']:
+                target_group['CURRENT_MEDICATION'].append(get_rdf_label(g_neuro_dkg, rdfs, response['hasCurrentMedication'])) 
+            if response['hasComorbidity']:
+                target_group['COMORBIDTY'].append(get_rdf_label(g_neuro_dkg, rdfs, response['hasComorbidity']))
+            if response['treatmentDuration']:
+                target_group['TREATMENT_DURATION'].append(get_rdf_label(g_neuro_dkg, rdfs, response['treatmentDuration'])) 
+            if response['hasSymptom']:
+                target_group['SYMPTOM'].append(get_rdf_label(g_neuro_dkg, rdfs, response['hasSymptom'])) 
+            if response['hasTherapy']:
+                target_group['THERAPY'].append(response['hasTherapy'])
+
+    return target_group
+
+def get_target_group(g_neuro_dkg, target_group_urls, ns1, rdfs):
+    target_group = {
+        'AGE_GROUP': [], 
+        'MIN_AGE': [], 
+        'TREATMENT_CODE': [], 
+        'SYMPTOM': [], 
+        'THERAPY': [], 
+        'CURRENT_MEDICATION': [], 
+        'TREATMENT_DURATION': [], 
+        'RESPONSE_STATUS': [], 
+        'COMORBIDTY': []
+    }
+    
+    for url in target_group_urls:
+        query = prepareQuery(
+            """
+            SELECT ?hasAgeGroup ?hasMinAge ?responseStatus ?hasCurrentMedication ?hasComorbidity ?hasTreatment ?treatmentDuration ?hasSymptom ?hasTherapy
+            WHERE {
+                OPTIONAL { <%s> ns1:neurodkg:hasAgeGroup ?hasAgeGroup }
+                OPTIONAL { <%s> ns1:neurodkg:hasMinAge ?hasMinAge }
+            
+                OPTIONAL { <%s> ns1:neurodkg:responseStatus ?responseStatus }
+                OPTIONAL { <%s> ns1:neurodkg:hasCurrentMedication ?hasCurrentMedication }
+                OPTIONAL { <%s> ns1:neurodkg:hasComorbidity ?hasComorbidity }
+                OPTIONAL { <%s> ns1:neurodkg:hasTreatment ?hasTreatment }
+
+                OPTIONAL { <%s> ns1:neurodkg:treatmentDuration ?treatmentDuration }
+                OPTIONAL { <%s> ns1:neurodkg:hasSymptom ?hasSymptom }
+                OPTIONAL { <%s> ns1:neurodkg:hasTherapy ?hasTherapy }
+            }
+            """ % (url, url, url, url, url, url, url, url, url),
+            initNs={"ns1": ns1}
+        )
+
+        for response in g_neuro_dkg.query(query):
+            if response['hasAgeGroup']:
+                target_group['AGE_GROUP'].append(response['hasAgeGroup']),
+            if response['hasMinAge']:
+                target_group['MIN_AGE'].append(response['hasMinAge']),
+            if response['responseStatus']:
+                target_group['RESPONSE_STATUS'].append(response['responseStatus']),
+            if response['hasCurrentMedication']:
+                target_group['CURRENT_MEDICATION'].append(get_rdf_label(g_neuro_dkg, rdfs, response['hasCurrentMedication'])),
+            if response['hasComorbidity']:
+                target_group['COMORBIDTY'].append(get_rdf_label(g_neuro_dkg, rdfs, response['hasComorbidity'])),
+            if response['treatmentDuration']:
+                target_group['TREATMENT_DURATION'].append(response['treatmentDuration']),
+            if response['hasSymptom']:
+                target_group['SYMPTOM'].append(get_rdf_label(g_neuro_dkg, rdfs, response['hasSymptom'])),
+            if response['hasTherapy']:
+                target_group['THERAPY'].append(get_rdf_label(g_neuro_dkg, rdfs, response['hasTherapy'])),
+            if response['hasTreatment']:
+                target_group['TREATMENT_CODE'].append(response['hasTreatment']),
+                get_treatment_group(g_neuro_dkg, target_group, ns1, rdfs)
+    
+    return target_group
+
+def extract(g_neuro_dkg, g_indications, output_csv):
     nkgi_indications = Namespace("http://www.w3id.org/neurodkg/Instances/")
     rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
     ns1 = Namespace("http://www.w3id.org/")
 
-    # Define the SPARQL query for neurodkg file
-    query_neurodkg = prepareQuery(
+    # The type/class DrugDiseaseTargetGroup can contain multiple disease and target groups
+    query_seperator = ','
+    query = prepareQuery(
         """
-        PREFIX ns1: <http://www.w3id.org/>
-        SELECT ?context ?drug ?disease ?targetGroup
+        SELECT ?context ?drug (GROUP_CONCAT(?disease; SEPARATOR="%s") AS ?diseases) (GROUP_CONCAT(?targetGroup; SEPARATOR="%s") AS ?targetGroups)
         WHERE {
             ?context a ns1:neurodkg:DrugDiseaseTargetGroup ;
-                     ns1:neurodkg:drug ?drug ;
-                     ns1:neurodkg:disease ?disease ;
-                     ns1:neurodkg:targetGroup ?targetGroup .
+                    ns1:neurodkg:drug ?drug ;
+                    ns1:neurodkg:disease ?disease .
+            OPTIONAL { ?context ns1:neurodkg:targetGroup ?targetGroup }
         }
-        """
+        GROUP BY ?context ?drug ?disease
+        """ % (query_seperator, query_seperator),
+        initNs={"ns1": ns1}
     )
 
+    rows = []
+    for result in g_neuro_dkg.query(query):
+        context_url = result["context"]
+        drug_url = result["drug"]
+        
+        context_no = context_url.split("/")[-1]
+        target_group_urls = result["targetGroups"].split(query_seperator)
+        disease_urls = result["diseases"].split(query_seperator)
+        
+        drug = get_rdf_label(g_neuro_dkg, rdfs, drug_url)
+        disease = get_diseases(g_neuro_dkg, rdfs, disease_urls)
+        target_group = get_target_group(g_neuro_dkg, target_group_urls, ns1, rdfs)
+        indication_text = get_indication_text(g_indications, context_no, nkgi_indications, rdfs)
+
+        row = {}
+        row['CONTEXT_NO'] = context_no 
+        row['DRUG'] = drug 
+        row['DISEASE'] = disease 
+        row['text'] = indication_text 
+
+        # Remove the treatment code
+        target_group.pop('TREATMENT_CODE')
+        
+        for key in target_group.keys():
+            if not target_group[key]:            
+                continue
+                
+            row[key] = set()
+            for value in target_group[key]:
+                row[key].add(str(value))
+
+            # If the set is consist of single entry, just add the row value as string 
+            if len(row[key]) == 1:
+                row[key] = target_group[key][0] 
+
+        rows.append(row)
+
     with open(output_csv, mode='w', newline='') as csv_file:
-        fieldnames = ['DRUG', 'CONDITION', 'TARGET_GROUP', 'text']
+        fieldnames = [
+            'CONTEXT_NO', 'DRUG', 'DISEASE', 'AGE_GROUP', 'MIN_AGE', 
+            'SYMPTOM', 'THERAPY', 'CURRENT_MEDICATION',
+            'TREATMENT_DURATION', 'RESPONSE_STATUS', 'COMORBIDTY', 'text']
+        
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-
-        for row in g_neuro_dkg.query(query_neurodkg):
-            context_url = row["context"]
-            drug_url = row["drug"]
-            disease_url = row["disease"]
-            target_group_url = row["targetGroup"]
-
-            indication_text = get_indication_text(g_indications, context_url, nkgi_indications, rdfs)
-            drug = get_drug_name(g_neuro_dkg, drug_url, ns1, rdfs)
-            disease = get_disease_name(g_neuro_dkg, disease_url, rdfs)
-            target_group = get_target_group(g_neuro_dkg, target_group_url, ns1)
-
-            # Write data to CSV file
-            writer.writerow({'DRUG': drug, 'CONDITION': disease, 'TARGET_GROUP': target_group, 'text': indication_text})
+        writer.writerows(rows)
 
 def load_rdf_graph(file_path):
     g = Graph()
@@ -120,16 +224,17 @@ def load_rdf_graph(file_path):
     return g
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Extract information from RDF files.")
-    parser.add_argument("neurodkg_file", help="Path to the neurodkg RDF file")
-    parser.add_argument("neurodkg__biolink_file", help="Path to the neurodkg biolink RDF file")
-    parser.add_argument("indications_file", help="Path to the indication text RDF file")
+    parser = argparse.ArgumentParser(description="Extract relations from RDF files and generate a CSV output.")
+    parser.add_argument("neurodkg_file", help="Path to the neurodkg RDF file (neuro_dkg.ttl)")
+    parser.add_argument("context_label_file", help="Path to the context label RDF file (context_label.ttl)")
     parser.add_argument("output_csv", help="Path to the output CSV file") 
     args = parser.parse_args()
 
-    # Load the RDF files into graphs
+    print("Loading RDF files...")
     g_neuro_dkg = load_rdf_graph(args.neurodkg_file)
-    g_neuro_dkg_biolink = load_rdf_graph(args.neurodkg__biolink_file)
-    g_indications = load_rdf_graph(args.indications_file)
+    g_indications = load_rdf_graph(args.context_label_file)
 
-    parse(g_neuro_dkg, g_neuro_dkg_biolink, g_indications, args.output_csv) 
+    print("Extracting data and generating CSV...")
+    extract(g_neuro_dkg, g_indications, args.output_csv)
+
+    print("CSV generation complete. Output saved to", args.output_csv)
